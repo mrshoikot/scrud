b0acc3ce9bb97389db5224154fe0a1c0195d1f5d
mrshoikot <mrshoikot@users.noreply.github.com>
2023-03-18 07:30:51 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Mrshoikot\Scrud\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Mrshoikot\Scrud\ScrudServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
